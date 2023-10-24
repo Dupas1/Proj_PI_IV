f5345f1871fa70eac414ce1ff094df70137a26b2
Briann Oliveira <104512517+BriannOliveira@users.noreply.github.com>
2023-10-23 21:00:40 -0300

    Create readMe.mk

---
+++ readMe.mk
@@ -0,0 +1,26 @@
+# Projeto Integrador IV do 4º Semestre - PUC-CAMPINAS
+
+## WoodPecker 🐦
+WoodPecker é um assistente inteligente que visa a melhora da saúde de sua memória para otimizar seu aprendizado. Seja você um estudante autodidata ou alguém buscando aumentar sua capacidade de memória, WoodPecker é sua ferramenta confiável.
+
+## Funcionalidades 🚀
+- **Login e Autenticação:** Acesso seguro através de um sistema robusto de login.
+
+- **Home Dashboard:** Visualize sua progressão, tarefas pendentes e desempenho geral.
+
+- **Anotações:** Faça anotações interativas e organize-as em categorias.
+
+- **StudySpace:** Espaço dedicado para estudo com flashcards, revisões e quizzes.
+
+- **Rede Neural:** Uma rede neural integrada para personalizar e otimizar seu plano de estudos.
+
+- **Performance/Report Overview:** Relatórios detalhados sobre seu progresso e áreas que precisam de mais foco.
+
+- **Profile:** Personalize seu perfil, adicione uma foto e atualize seus detalhes.
+
+- **Configurações:** Ajuste as configurações do aplicativo de acordo com suas preferências.
+
+## Tecnologias 🛠
+- **Backend:** Java, Javascript
+- **Banco de Dados:** MongoDB
+- **Frontend:** React, JavaScript
