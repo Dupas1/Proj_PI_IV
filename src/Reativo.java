4b03bbd9e5bd790d6f90e4a71975cbeb6a439ff8
Gabriel de Arruda Dupas <gabrieldupas14@gmail.com>
2023-10-24 22:04:14 -0300

    teste2

---
+++ src/Reativo.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.net.*;
+
+public class Reativo {
+    public static void main (String[] args) throws IOException {
+
+        try
+        {
+            ServerSocket pedido = new ServerSocket(12345);
+
+            //fio do telefone
+            Socket conexao = pedido.accept();//1 ponta do fio
+
+            ObjectInputStream receptor = new ObjectInputStream(conexao.getInputStream()); //fone
+
+            //ObjectOutputStream transmissor = new ObjectOutputStream(conexao.getOutputStream());//microfone,porem neste programa ele não é necessario
+
+            String texto;
+
+            do {
+                texto = (String)receptor.readObject();//pegou a informação do proativo// tenho que falar que é estring pelo receptior ser object
+                System.out.println(texto);
+            }while(!texto.toUpperCase().equals("FIM"));
+
+            //transmissor.close();
+            receptor.close();
+            conexao.close();
+            pedido.close();
+        }
+        catch (Exception erro)
+        {
+            System.err.println(erro.getMessage());
+        }
+
+    }
+}
