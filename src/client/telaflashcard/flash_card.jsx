be964fcca2f06c73c1fab2b4a5ce760b227cd405
FabricioMartinss <fabricio.martello.martins@gmail.com>
2023-11-22 16:28:31 -0300

    tela flash-cards

---
+++ src/client/telaflashcard/flash_card.jsx
@@ -0,0 +1,59 @@
+export default function FlashCard(){
+    return(
+        <div className="allfc">
+            <section className="containersfc"
+            ><div className="container1fc">
+                    
+                </div>
+            </section>
+            <footer className="footer">
+                <div className="imgesquerda">
+                    <div>
+                        <Link to="/telaconfig">
+                            <img className="img" src="icone.config.png" alt="" />
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to="/profile">
+                            <img className="img" src="icone.perfil.png" alt="" />
+                        </Link>
+                    </div>
+                    <div>
+                        <a href="">
+                            <img className="img" src="icone.lua.png" alt="" />
+                        </a>
+                    </div>
+                </div>
+                <div className="imgcenter">
+                    <div>
+                        <Link to="/studyspace">
+                            <img className="img" src="icone.doc.png" alt="" />
+                        </Link>
+                    </div>
+                    <div>
+                        <a href="">
+                            <img className="img" src="icone.lista.png" alt="" />
+                        </a>
+                    </div>
+                    <div>
+                        <a href="">
+                            <img className="img" src="icone.trevo.png" alt="" />
+                        </a>
+                    </div>
+                    <div>
+                        <a href="">
+                            <img className="img" src="icone.bandeira.png" alt="" />
+                        </a>
+                    </div>
+                </div>
+                <div className="imgdireita">
+                    <a href="">
+                        <Link to="/telahome">
+                            <img className="img" src="icone.wp.png" alt="" />
+                        </Link>
+                    </a>
+                </div>
+            </footer>
+        </div>
+    )
+}
