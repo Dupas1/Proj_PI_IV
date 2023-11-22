227d6b82f9d31040e0dd8d1f5f485f936dde7ef8
Felipe Arruda Dupas <118365943+FelipeDupas@users.noreply.github.com>
2023-11-22 19:27:27 -0300

    Funcionalidades de retorno realizadas

--- src/client/telaflashcard/flash_card.jsx
+++ src/client/telaflashcard/flash_card.jsx
@@ -1,73 +1,56 @@
 import { Link } from 'react-router-dom';
 
-export default function FlashCard(){
-    return(
+export default function FlashCard() {
+    return (
         <div className="allfc">
             <section className="containersfc">
-            <div className="container1fc">
-                <div className="header">
-                <div className="para_hojefc">
-                    <a href="">
-                        <img className="imgvoltar"src="Voltar.PNG" alt="" />
-                    </a>
-                    <h1 className="titlesfc">Mongo DB</h1>
+                <div className="container1fc">
+                    <div className="header">
+                        <div className="para_hojefc">
+                            <Link to="/studyspace">
+                                <img className="imgvoltar" src="Voltar.PNG" alt="Voltar" />
+                            </Link>
+                            <h1 className="titlesfc">Mongo DB</h1>
+                        </div>
+                        <div className="buttonfc">
+                            <button className="button2fc">
+                                <h2>Adicionar FlashCard</h2>
+                            </button>
+                        </div>
+                    </div>
+                    <hr />
                 </div>
-                <div className="buttonfc">
-                    <button className="button2fc">
-                        <h2 >Adicionar FlashCard</h2>
-                    </button>
-                </div> 
-                </div> 
-                <hr />    
-            </div>
             </section>
             <footer className="footer">
                 <div className="imgesquerda">
-                    <div>
-                        <Link to="/telaconfig">
-                            <img className="img" src="icone.config.png" alt="" />
-                        </Link>
-                    </div>
-                    <div>
-                        <Link to="/profile">
-                            <img className="img" src="icone.perfil.png" alt="" />
-                        </Link>
-                    </div>
-                    <div>
-                        <a href="">
-                            <img className="img" src="icone.lua.png" alt="" />
-                        </a>
-                    </div>
-                </div>
-                <div className="imgcenter">
-                    <div>
-                        <Link to="/studyspace">
-                            <img className="img" src="icone.doc.png" alt="" />
-                        </Link>
-                    </div>
-                    <div>
-                        <Link to = "/flashCard">
-                            <img className="img" src="icone.lista.png" alt="" />
-                        </Link>
-                    </div>
-                    <div>
-                        <a href="">
-                            <img className="img" src="icone.trevo.png" alt="" />
-                        </a>
-                    </div>
-                    <div>
-                        <a href="">
-                            <img className="img" src="icone.bandeira.png" alt="" />
-                        </a>
-                    </div>
-                </div>
-                <div className="imgdireita">
+                    <Link to="/telaconfig">
+                        <img className="img" src="icone.config.png" alt="Configurações" />
+                    </Link>
+                    <Link to="/profile">
+                        <img className="img" src="icone.perfil.png" alt="Perfil" />
+                    </Link>
                     <a href="">
-                        <Link to="/telahome">
-                            <img className="img" src="icone.wp.png" alt="" />
-                        </Link>
+                        <img className="img" src="icone.lua.png" alt="Lua" />
                     </a>
                 </div>
+                <div className="imgcenter">
+                    <Link to="/studyspace">
+                        <img className="img" src="icone.doc.png" alt="Estudos" />
+                    </Link>
+                    <Link to="/flashCard">
+                        <img className="img" src="icone.lista.png" alt="Lista" />
+                    </Link>
+                    {/* Replace with valid href or convert to Link */}
+                    <a href="">
+                        <img className="img" src="icone.trevo.png" alt="Trevo" />
+                    </a>
+                    <a href="">
+                        <img className="img" src="icone.bandeira.png" alt="Bandeira" />
+                    </a>
+                </div>
+                <Link to="/telahome">
+                    <img className="img" src="icone.wp.png" alt="Home" />
+                </Link>
             </footer>
         </div>
     )
