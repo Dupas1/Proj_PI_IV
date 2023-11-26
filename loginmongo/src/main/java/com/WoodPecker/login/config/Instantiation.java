59ae5bd547020057752e6d838dc93e637d5e7b18
Gabriel de Arruda Dupas <118358608+Dupas1@users.noreply.github.com>
2023-11-25 22:56:51 -0300

    Projecao dos dados do Usuario com DTO

--- loginmongo/src/main/java/com/WoodPecker/login/config/Instantiation.java
+++ loginmongo/src/main/java/com/WoodPecker/login/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.WoodPecker.login.domain.FlashCard;
 import com.WoodPecker.login.domain.User;
+import com.WoodPecker.login.dto.IdUserDTO;
 import com.WoodPecker.login.repository.FlashCardRepository;
 import com.WoodPecker.login.repository.UserRepository;
 
@@ -34,10 +35,11 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green","masc","alex@gmail.com","senha5","phone",LocalDate.now());
 		User bob = new User(null, "Bob Grey","24","bob@gmail.com","senha6","phone",LocalDate.now());
 		
-		FlashCard flashcard1 = new FlashCard(null,"Qual é o seu nome ?",null,"Pessoal",null,sdf.parse("21/11/2023"),1,maria);
-		FlashCard flashcard2 = new FlashCard(null,"Qual é o nome da sua mãe ?",null,"Pessoal",null,sdf.parse("20/11/2023"),1,alex);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		FlashCard flashcard1 = new FlashCard(null,"Qual é o seu nome ?",null,"Pessoal",null,sdf.parse("21/11/2023"),1,new IdUserDTO(maria));
+		FlashCard flashcard2 = new FlashCard(null,"Qual é o nome da sua mãe ?",null,"Pessoal",null,sdf.parse("20/11/2023"),1,new IdUserDTO(alex));
+		
 		FlashCardRepository.saveAll(Arrays.asList(flashcard1,flashcard2));
 		
 	}
